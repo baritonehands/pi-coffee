3972baa4cca582b02fd3752c43d030ace569f9a9
Brian Gregg <biscuitalmighty@gmail.com>
2013-04-13 13:51:16 -0500

    Open USB scale on run.

--- coffee.py
+++ coffee.py
@@ -6,4 +6,6 @@ def index():
     return render_template('coffee.html', weight='4 lbs, 10.7 oz')
 
 if __name__ == '__main__':
-    app.run()
+    global hid
+    with open('/dev/usb/hiddev0', 'rb') as hid:
+        app.run()
