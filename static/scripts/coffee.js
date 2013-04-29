6deecc9f451675729dcf7d8925986fd057295a4b
Brian Gregg <biscuitalmighty@gmail.com>
2013-04-29 17:35:33 -0500

    Added cups to text, show time using Moment.

--- static/scripts/coffee.js
+++ static/scripts/coffee.js
@@ -9,26 +9,28 @@ MstarCoffee = (function() {
         $.getJSON('/stats', function(data) {
             self.weight(data.weight);
             self.percent(data.percent);
-            self.updated(new Date());
+            self.updated(moment().format('LLLL'));
         });
     };
     
     self.ctx = Raphael('coffee', 200, 400);
 
     self.liquid = self.ctx.rect(0, 0, 200, 400, 10);
-    self.liquid.attr({fill: 'brown', stroke: 'transparent'});
-
-    self.label = self.ctx.text(100, 20, '100%');
-    self.label.attr({'font-size': '20px', fill: '#FFF'});
+    self.liquid.attr({fill: '#362819', stroke: 'transparent'});
+    
+    var lblOffset = 40;
+    self.label = self.ctx.text(100, lblOffset, '100%');
+    self.label.attr({'font-size': '20px', fill: '#CCBEAF'});
     
     self.urn = self.ctx.rect(0, 0, 200, 400, 10);
+    self.urn.attr({stroke: '#362819'});
 
     self.percent.subscribe(function(percent) {
         var lAnim = Raphael.animation({height:(percent / 100) * 400, y: (1 - (percent / 100)) * 400}, 600, '<>');
-        var txtAnim = Raphael.animation({y: (1 - (percent / 100)) * 400 + 20}, 600, '<>');
+        var txtAnim = Raphael.animation({y: (1 - (percent / 100)) * 400 + lblOffset}, 600, '<>');
         self.liquid.animate(lAnim);
         self.label.animateWith(self.liquid, lAnim, txtAnim);
-        self.label.attr({text: percent + '%'});
+        self.label.attr({text: percent + '%\n' + (64 * percent / 100) + ' cups'});
     });
     
     getStats();
